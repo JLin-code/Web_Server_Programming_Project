6969d300d1b9e55189e20a5ed7c923a68f29cc90
Jack Lin <lin_jack@myyahoo.com>
2025-03-08 18:58:29 -0500

    Changed Signup for WIP

--- client/src/views/SignUpView.vue
+++ client/src/views/SignUpView.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+const page = 'Sign Up'
 
-var page = 'Sign Up'
 </script>
 
 <template>
-  <main>
-    <h1 class="title">Welcome to {{ page }}</h1>
+  <main class="container section">
+    <div class="notification is-warning">
+      <h1 class="title">{{ page }} - Coming Soon!</h1>
+      <p>Sign up functionality will be implemented in a future update.</p>
+      <p>Please use the login option in the navigation bar for now.</p>
+    </div>
   </main>
 </template>

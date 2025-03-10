f4d9cf1877fc0e471a82dd88e676b79a483a6c6a
Jack Lin <lin_jack@myyahoo.com>
2025-03-10 13:38:33 -0400

    Revert "error handling"
    This reverts commit 965869b6884f62d94ea11186bf82d56f115f855e.

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,12 +6,6 @@ import router from './router'
 
 const app = createApp(App)
 
-// Add global error handler for better debugging
-app.config.errorHandler = (err, instance, info) => {
-  console.error('Vue Error:', err)
-  console.error('Error Info:', info)
-}
-
 app.use(router)
 
 app.mount('#app')

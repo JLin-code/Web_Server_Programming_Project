b13276c91c0f2c358ceae9ff06d9f0a9290bf181
Jack Lin <lin_jack@myyahoo.com>
2025-03-09 21:29:38 -0400

    People Search Page WIP

--- client/src/views/PeopleSearchView.vue
+++ client/src/views/PeopleSearchView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+const page = 'People Search'
 
-var page = 'People Search'
 </script>
 
 <template>
-  <main>
-    <h1 class="title">Welcome to {{ page }}</h1>
+  <main class="container section">
+    <div class="notification is-warning">
+      <h1 class="title">{{ page }} - Coming Soon!</h1>
+      <p>People Search functionality will be implemented in a future update.</p>
+    </div>
   </main>
 </template>

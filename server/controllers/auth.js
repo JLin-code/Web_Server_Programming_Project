5e42749af8e40681a535f9794c58b1aefa8087b6
Jack Lin <lin_jack@myyahoo.com>
2025-04-30 23:11:52 -0400

    changed bcrypt to bcryptjs

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Changed from bcrypt to bcryptjs
 const jwt = require('jsonwebtoken');
 const { CustomError, statusCodes } = require('../models/errors');
 const userModel = require('../models/users');

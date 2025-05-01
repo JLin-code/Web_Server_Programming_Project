92a6ae2b39aeac3229b11bc225e61084ce3731dc
Jack Lin <lin_jack@myyahoo.com>
2025-04-30 23:09:12 -0400

    fixing issues

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -14,7 +14,7 @@ export function isAuthenticated() {
   try {
     const payload = JSON.parse(atob(token.split('.')[1]));
     return payload.exp > Date.now() / 1000;
-  } catch (e) {
+  } catch {
     return false;
   }
 }
